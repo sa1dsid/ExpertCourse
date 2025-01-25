ba8b52e3a63ed75e2229f03c87c1437c72071611
sa1dsid <worming.said@gmail.com>
2025-01-25 23:25:06 +0300

    UG-02 Page object

--- app/src/androidTest/java/com/example/expertcourse/ScenarioTest.kt
+++ app/src/androidTest/java/com/example/expertcourse/ScenarioTest.kt
@@ -3,6 +3,7 @@ package com.example.expertcourse
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.expertcourse.game.GamePage
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -17,7 +18,7 @@ class ScenarioTest {
     @get:Rule
     val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
-    private lateinit var gamePage: GamePage()
+    private lateinit var gamePage: GamePage
 
     @Before
     fun setup(){
@@ -27,9 +28,9 @@ class ScenarioTest {
     /**
      * UG-01
      */
+
     @Test
     fun testcase1() {
-        val gamePage = GamePage(word = "animal".reversed())
 
         gamePage.assertInitialState()
 
@@ -41,13 +42,18 @@ class ScenarioTest {
 
         gamePage.clickNext()
     }
+
+    /**
+     * UG-02
+     */
+
+    @Test
     fun testcase2() {
-        val gamePage = GamePage(word = "animal".reversed())
 
         gamePage.assertInitialState()
 
         gamePage.input(text = "anima")
-        gamePage.assertSufficientInputState()
+        gamePage.insufficientInputState()
 
         gamePage.clickCheck()
         gamePage.assertIncorrectState()
@@ -57,9 +63,15 @@ class ScenarioTest {
 
         gamePage.clickCheck()
         gamePage.assertCorrectState()
+
     }
+
+    /**
+     * UG-03
+     */
+
+    @Test
     fun testcase3() {
-        val gamePage = GamePage(word = "animal".reversed())
 
         gamePage.assertInitialState()
 
@@ -70,6 +82,6 @@ class ScenarioTest {
         gamePage.assertIncorrectState()
 
         gamePage.clickSkip()
-        gamePage.InitialState()
+        gamePage.initialState()
     }
 }
